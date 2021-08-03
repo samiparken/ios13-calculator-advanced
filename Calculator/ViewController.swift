c06a620dfc61eb7c128e61d5c6633d0fb842eef3
Han-Saem Park <samiparken@gmail.com>
2021-08-03 10:04:52 +0200

    initial settings

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,31 +1,28 @@
-//
-//  ViewController.swift
-//  Calculator
-//
-//  Created by Angela Yu on 10/09/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+//MARK: - Buttons
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
-    
+            
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a number is entered into the keypad
-    
-    }
+            
+        if let numValue = sender.currentTitle {
+            
+            switch (displayLabel.text) {
+            case "0":
+                displayLabel.text = numValue
+            default:
+                displayLabel.text = displayLabel.text! + numValue
+            }
 
+        }
+    }
 }
 

24f55f0d60f8c096ed1beb7462d6d5bdd44c66cf
Han-Saem Park <samiparken@gmail.com>
2021-08-03 11:14:08 +0200

    numbers and AC, +/-, %

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -4,24 +4,40 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var isFinishedTypingNumber: Bool = true
+    
 //MARK: - Buttons
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-            
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert displayLabel.text to a Double")
+        }  // for testing and catching where the error happens
+
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * (-1))
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String( number / 100 )
+            }
+        }
+        
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
             
         if let numValue = sender.currentTitle {
-            
-            switch (displayLabel.text) {
-            case "0":
+            if isFinishedTypingNumber {
                 displayLabel.text = numValue
-            default:
+                isFinishedTypingNumber = false
+            } else {
                 displayLabel.text = displayLabel.text! + numValue
             }
-
         }
     }
 }

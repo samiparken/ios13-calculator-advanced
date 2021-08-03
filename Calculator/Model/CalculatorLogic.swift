68d922fadacee6dde513ef14abca5a02029165e8
Han-Saem Park <samiparken@gmail.com>
2021-08-03 16:39:58 +0200

    complete the project

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -12,16 +12,12 @@ struct CalculatorLogic {
     
     mutating func calculate(symbol: String) -> Double? {
         if let n = number {
-            if symbol == "+/-" {
-                return n * -1
-            } else if symbol == "AC" {
-                return 0
-            } else if symbol == "%" {
-                return n * 0.01
-            } else if symbol == "=" {
-                return performTwoNumCalculation(n2: n)
-            } else {
-                intermediateCalculation = (n1: n, calcMethod: symbol)
+            switch symbol {
+                case "+/-": return n * -1
+                case "AC":  return 0
+                case "%":   return n * 0.01
+                case "=":   return performTwoNumCalculation(n2: n)
+                default: intermediateCalculation = (n1: n, calcMethod: symbol)
             }
         }
         return nil
